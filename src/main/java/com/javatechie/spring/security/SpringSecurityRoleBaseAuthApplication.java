d8f4602c6f2e6193a7d6ace026f3c997b3704769
basahota <techbasanta757@gmail.com>
2020-11-19 23:56:16 +0530

    Enable permission

--- src/main/java/com/javatechie/spring/security/SpringSecurityRoleBaseAuthApplication.java
+++ src/main/java/com/javatechie/spring/security/SpringSecurityRoleBaseAuthApplication.java
@@ -1,13 +1,18 @@
 package com.javatechie.spring.security;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "User Auth", version = "2.0", description = "Group-Management Microservice")
+)
 public class SpringSecurityRoleBaseAuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityRoleBaseAuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityRoleBaseAuthApplication.class, args);
+    }
 
 }
--- src/main/java/com/javatechie/spring/security/controller/UserController.java
+++ src/main/java/com/javatechie/spring/security/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.javatechie.spring.security.controller;
 
+import com.javatechie.spring.security.common.UserConstant;
 import com.javatechie.spring.security.entity.User;
 import com.javatechie.spring.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/user")
@@ -23,17 +27,21 @@ public class UserController {
     @PostMapping("/add")
     public User addUser(@RequestBody User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles("ROLE_USER");
+        user.setRoles(UserConstant.DEFAULT_ROLE);
         return repository.save(user);
     }
 
-    @GetMapping("/changeAccess/{userId}")
-   /* @Secured("ROLE_ADMIN")*/
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public String provideAdminAccess(@PathVariable int userId, Principal principal) {
+    @GetMapping("/giveAccess/{userId}/{roleType}")
+    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN') or hasAuthority('ROLE_MODERATOR')")
+    public String provideAdminAccess(@PathVariable int userId, @PathVariable String roleType, Principal principal) {
+        String assignRole = "";
         User user = repository.findById(userId).get();
-        String assignRole = user.getRoles() + "," + "ROLE_ADMIN";
-        user.setRoles(assignRole);
+        List<String> activeRoles = getRolesByLoggedInUser(principal);
+        if (activeRoles.contains(roleType)) {
+            assignRole = user.getRoles() + "," + roleType;
+            user.setRoles(assignRole);
+        }
         repository.save(user);
         return "Hi " + user.getUserName() + " ADMIN access provided to you by " + principal.getName();
     }
@@ -51,4 +59,22 @@ public class UserController {
     public String testUserAccess() {
         return "user can only access this !";
     }
+
+    private List<String> getRolesByLoggedInUser(Principal principal) {
+        String loggedInUserRoles = getLoggedInUser(principal).getRoles();
+        List<String> roles = Arrays.stream(loggedInUserRoles.split(","))
+                .collect(Collectors.toList());
+        if (roles.contains("ROLE_ADMIN")) {
+            return Arrays.stream(UserConstant.ADMIN_ACCESS).collect(Collectors.toList());
+        }
+        if (roles.contains("ROLE_MODERATOR")) {
+            return Arrays.stream(UserConstant.MODERATOR_ACCESS).collect(Collectors.toList());
+        }
+        return Collections.emptyList();
+    }
+
+    private User getLoggedInUser(Principal principal) {
+        return repository.findByUserName(principal.getName()).get();
+    }
+
 }
